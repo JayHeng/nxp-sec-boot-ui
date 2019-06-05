83aca1dd2255557cb9e2734f93c36e347e3f42ff
Jay Heng <jie.heng@nxp.com>
2019-06-05 12:53:41 +0800

    [FW] Add ROM patch(v2.1) for RT600 A0
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

---
+++ src/targets/MIMXRT685/A0_patch/RT600_ROM_A0_Patch_v2.1.sh
@@ -0,0 +1,51 @@
+BLHOST="./blhost.exe -p COM150,230400"
+
+$BLHOST -- efuse-program-once 0xA1  55010003
+$BLHOST -- efuse-program-once 0xA2  1300A7A4
+$BLHOST -- efuse-program-once 0xA3  0010F2C5
+$BLHOST -- efuse-program-once 0xA4  130072d8
+$BLHOST -- efuse-program-once 0xA5  07C00400
+$BLHOST -- efuse-program-once 0xA6  1300838c
+$BLHOST -- efuse-program-once 0xA7  47704770
+
+$BLHOST -- efuse-program-once 0xA8  55010003
+$BLHOST -- efuse-program-once 0xA9  13004240
+$BLHOST -- efuse-program-once 0xAA  0400f2c0
+$BLHOST -- efuse-program-once 0xAB  13005c74
+$BLHOST -- efuse-program-once 0xAC  21010000
+$BLHOST -- efuse-program-once 0xAD  1301ee08
+$BLHOST -- efuse-program-once 0xAE  0200f240
+
+$BLHOST -- efuse-program-once 0xAF  55020103
+$BLHOST -- efuse-program-once 0xB0  13019c63
+$BLHOST -- efuse-program-once 0xB1  4400F240
+$BLHOST -- efuse-program-once 0xB2  0400F6C0
+$BLHOST -- efuse-program-once 0xB3  47709400
+
+$BLHOST -- efuse-program-once 0xB4  55020102
+$BLHOST -- efuse-program-once 0xB5  130046d3
+$BLHOST -- efuse-program-once 0xB6  72fff64b
+$BLHOST -- efuse-program-once 0xB7  47709202
+
+$BLHOST -- efuse-program-once 0xB8  5502010d
+$BLHOST -- efuse-program-once 0xB9  130073b5
+$BLHOST -- efuse-program-once 0xBA  137cf240
+$BLHOST -- efuse-program-once 0xBB  0313f2c5
+$BLHOST -- efuse-program-once 0xBC  f242681a
+$BLHOST -- efuse-program-once 0xBD  f2c12000
+$BLHOST -- efuse-program-once 0xBE  2a000000
+$BLHOST -- efuse-program-once 0xBF  6a5bbf01
+$BLHOST -- efuse-program-once 0xC0  f64f2b00
+$BLHOST -- efuse-program-once 0xC1  600171ff
+$BLHOST -- efuse-program-once 0xC2  305af24c
+$BLHOST -- efuse-program-once 0xC3  20c3f6c5
+$BLHOST -- efuse-program-once 0xC4  98069000
+$BLHOST -- efuse-program-once 0xC5  0002f100
+$BLHOST -- efuse-program-once 0xC6  47709006
+
+$BLHOST -- efuse-program-once 0xC7  55020103
+$BLHOST -- efuse-program-once 0xC8  130133bb
+$BLHOST -- efuse-program-once 0xC9  1e416a38
+$BLHOST -- efuse-program-once 0xCA  0fc94189
+$BLHOST -- efuse-program-once 0xCB  47709100
+
